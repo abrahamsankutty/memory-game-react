baf586227986dad0e78acd85dfc6b30843314244
Abraham Sankutty <abrahamsankutty@gmail.com>
2023-07-05 20:15:58 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,111 @@
+import { useEffect, useMemo, useState } from "react";
+import Confetti from "react-confetti";
+
+import "./App.css";
+const gameIcons = ["🏰", "🧝‍♂️", "💂‍♀️", "👩🏽‍🚒", "🧙‍♂️", "👺", "🤖", "🐯", "🦕"];
+function App() {
+  const [pieces, setPieces] = useState([]);
+  const isGameCompleted = useMemo(() => {
+    if (pieces.length > 0 && pieces.every((piece) => piece.solved)) {
+      return true;
+    }
+    return false;
+  }, [pieces]);
+  const startGame = () => {
+    const duplicateGameIcons = gameIcons.concat(gameIcons);
+    const newGameIcons = [];
+    while (newGameIcons.length < gameIcons.length * 2) {
+      const randomIndex = Math.floor(Math.random() * duplicateGameIcons.length);
+      newGameIcons.push({
+        emoji: duplicateGameIcons[randomIndex],
+        flipped: false,
+        solved: false,
+        position: newGameIcons.length,
+      });
+      duplicateGameIcons.splice(randomIndex, 1);
+    }
+    setPieces(newGameIcons);
+  };
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  const handleActive = (data) => {
+    const flippedData = pieces.filter((data) => data.flipped && !data.solved);
+    if (flippedData.length === 2) return;
+    const newPieces = pieces.map((piece) => {
+      if (piece.position === data.position) {
+        piece.flipped = !piece.flipped;
+      }
+      return piece;
+    });
+    setPieces(newPieces);
+  };
+
+  const gameLogicForFlipped = () => {
+    const flippedData = pieces.filter((data) => data.flipped && !data.solved);
+    if (flippedData.length === 2) {
+      setTimeout(() => {
+        if (flippedData[0].emoji === flippedData[1].emoji) {
+          setPieces(
+            pieces.map((piece) => {
+              if (
+                piece.position === flippedData[0].position ||
+                piece.position === flippedData[1].position
+              ) {
+                piece.solved = true;
+              }
+              return piece;
+            })
+          );
+        } else {
+          setPieces(
+            pieces.map((piece) => {
+              if (
+                piece.position === flippedData[0].position ||
+                piece.position === flippedData[1].position
+              ) {
+                piece.flipped = false;
+              }
+              return piece;
+            })
+          );
+        }
+      }, 800);
+    }
+  };
+
+  useEffect(() => {
+    gameLogicForFlipped();
+  }, [pieces]);
+
+  return (
+    <>
+      <h1 className="heading">Memory Game in react</h1>
+      <div className="container">
+        {pieces.map((data, index) => (
+          <div
+            className={`flip-card ${
+              data.flipped || data.solved ? "active" : ""
+            }`}
+            key={index}
+            onClick={() => handleActive(data)}
+          >
+            <div className="flip-card-inner">
+              <div className="flip-card-front"></div>
+              <div className="flip-card-back">{data.emoji}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+      {isGameCompleted && (
+        <div className="game-completed">
+          <div>YOU WIN!!!</div>
+          <Confetti width={window.innerWidth} height={window.innerHeight} />
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
